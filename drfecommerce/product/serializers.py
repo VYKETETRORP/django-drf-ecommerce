b4e0e9f4e89259066af4207f5775101c861904ad
VYKETETRORP <lesleygs56@gamil.com>
2024-03-06 15:05:18 +0700

    Implement Product

--- drfecommerce/product/serializers.py
+++ drfecommerce/product/serializers.py
@@ -14,6 +14,8 @@ class BrandSerializer(serializers.ModelSerializer):
     
 
 class ProductSerializer(serializers.ModelSerializer):
+  brand = BrandSerializer()
+  category = CategorySerializer()
   class Meta:
     model = Product
     fields = "__all__"    
